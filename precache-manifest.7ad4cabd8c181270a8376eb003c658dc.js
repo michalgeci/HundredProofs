05d05227dff8a4ea12d3767f9572f8c68104c5af
Michal Geci <michal.geci.1995@gmail.com>
2020-08-29 00:05:10 +0200

    Updates

---
+++ precache-manifest.7ad4cabd8c181270a8376eb003c658dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c1940d217727726a3007b1066abea3",
+    "url": "/HundredProofs/index.html"
+  },
+  {
+    "revision": "728ccaf4209a8cf15096",
+    "url": "/HundredProofs/static/css/2.81082e33.chunk.css"
+  },
+  {
+    "revision": "31adabc9c069545675cf",
+    "url": "/HundredProofs/static/css/main.7feebfc0.chunk.css"
+  },
+  {
+    "revision": "728ccaf4209a8cf15096",
+    "url": "/HundredProofs/static/js/2.065bd717.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/HundredProofs/static/js/2.065bd717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31adabc9c069545675cf",
+    "url": "/HundredProofs/static/js/main.aa0af13a.chunk.js"
+  },
+  {
+    "revision": "15f9e0024c90946697cf",
+    "url": "/HundredProofs/static/js/runtime-main.daa6ff84.js"
+  },
+  {
+    "revision": "21bb70b62317f276f2e97a919ff5bd8c",
+    "url": "/HundredProofs/static/media/BebasNeue-Regular.21bb70b6.ttf"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/HundredProofs/static/media/PatrickHand-Regular.eeb4dad6.ttf"
+  },
+  {
+    "revision": "216d43ee8707910af457af569eda1dec",
+    "url": "/HundredProofs/static/media/Quicksand-Regular.216d43ee.ttf"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/HundredProofs/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/HundredProofs/precache-manifest.7ad4cabd8c181270a8376eb003c658dc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HundredProofs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
